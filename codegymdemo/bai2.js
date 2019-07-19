ac1f8d5e408e081fe923c3dc897596c4edbc4a9f
ledinhthoa <ledaiphong6@gmail.com>
2019-07-19 15:28:43 +0700

    add .

---
+++ codegymdemo/bai2.js
@@ -0,0 +1,13 @@
+use={
+    firstname:'firstname',
+    lastname:'lastname',
+    showname:function () {
+       console.log(this.firstname+''+lastname)
+       
+    }
+ }
+ khoa={
+    firstname:'thoa',
+    lastname:'dinh',
+ }
+     
